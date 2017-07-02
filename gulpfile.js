4784da0323e9d021ff695ee46dbec83fe112e126
James Langford <derivative7117@live.com>
2017-07-01 21:22:55 -0600

    updated gulp-file

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
+
 const gulp = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass'),
-    concat = require('gulp-concat'),
-    CacheBuster = require('gulp-cachebust'),
-    cachebust = new CacheBuster(),
-    print = require('gulp-print'),
-    ngAnnotate = require('gulp-ng-annotate'),
-    htmlmin = require('gulp-htmlmin'),
-    uglify = require('gulp-uglify'),
-    cssmin = require('gulp-cssmin'),
-    gulpBrowser = require("gulp-browser"),
+    $ = require('gulp-load-plugins')({
+        pattern: ['gulp-*'],
+        replaceString: /\bgulp[\-.]/,
+        lazy: true,
+        camelize: true
+    }),
+    cachebust = new $.cachebust(),
     transforms = [{
         transform: "babelify",
         options: {presets: ["es2015"]}
@@ -21,40 +18,39 @@ gulp.task('icons', () => {
 })
 
 gulp.task('build-js', function () {
-    let stream = gulp.src('./client/js/**/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(gulpBrowser.browserify(transforms))
-        .pipe(print())
-        .pipe(concat('bundle.min.js'))
-        .pipe(ngAnnotate())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
+    return gulp.src('./client/js/**/*.js')
+        .pipe($.sourcemaps.init())
+        .pipe($.browser.browserify(transforms))
+        .pipe($.print())
+        .pipe($.concat('bundle.min.js'))
+        .pipe($.ngAnnotate())
+        .pipe($.uglify())
+        .pipe($.sourcemaps.write('./'))
         .pipe(gulp.dest('./dist/js'))
-    return stream
 })
-
 
 gulp.task('build-css', () => {
     return gulp.src('./client/styles/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(print())
-        .pipe(sass())
+        .pipe($.sourcemaps.init())
+        .pipe($.print())
+        .pipe($.sass())
         .pipe(cachebust.resources())
-        .pipe(concat('styles.min.css'))
-        .pipe(cssmin())
-        .pipe(sourcemaps.write('./maps'))
+        .pipe($.concat('styles.min.css'))
+        .pipe($.cssmin())
+        .pipe($.sourcemaps.write('./maps'))
         .pipe(gulp.dest('./dist/styles'))
 })
 
 gulp.task('views', () => {
     return gulp.src('./client/views/**/*')
-        .pipe(print())
-        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe($.print())
+        .pipe($.htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./dist/views'))
 })
 
-gulp.task('build', ['views', 'build-css', 'build-js', 'icons'], () => {
+gulp.task('build', ['build-js', 'build-css', 'views', 'icons'], () => {
     return gulp.src('./client/index.html')
+        .pipe($.htmlmin({collapseWhitespace: true}))
         .pipe(cachebust.references())
         .pipe(gulp.dest('dist'))
 })
