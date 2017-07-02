32d87298dc0e8cfb46cc5d9cf45dfa5a145c0ef3
James Langford <derivative7117@live.com>
2017-07-02 14:11:58 -0600

    updated filter function

--- filterMulti.js
+++ filterMulti.js
@@ -1,6 +1,6 @@
 module.exports = {
     filterMultiDimArr : arr => {         //function to filter duplicate arrays from array
-        return arr.filter(function (item) {
+        return arr.filter(item => {
             if (!this.hasOwnProperty(item[1])) {
                 return this[item[1]] = true
             }
